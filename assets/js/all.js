96633b481c19c1acd2a5be80a6f2cbb9bb35efc7
ZweigDen <65008094+ZweigDen@users.noreply.github.com>
2021-05-18 23:04:08 +0800

    master

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,43 +1,64 @@
-function swiper(){
-    var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 1,
-        centeredSlides: false,
-        spaceBetween: 0,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            320: {
-              slidesPerView: 1.2,
-              spaceBetween: 20,
-            },
-            767: {
-              slidesPerView: 2.5,
-              spaceBetween: 20,
-            },
-            1024: {
-              slidesPerView: 4.2,
-              spaceBetween: 30,
-            },
-          }
-      });
+function currSwiper(){
+  var swiper = new Swiper(".currSwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    effect: "fade",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+function swiper() {
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    centeredSlides: false,
+    spaceBetween: 0,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: false,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2.3,
+        spaceBetween: 20,
+      },
+      766: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4.2,
+        spaceBetween: 30,
+      },
+    }
+  });
 }
 // menuBtn
-function btn(){
+function btn() {
   var menuButton = document.querySelector(".menu-button");
-        menuButton.addEventListener("click",myPrev);
-        function myPrev(){
-            $('.menu-button').toggleClass("cross bgBlack");
-        }
+  menuButton.addEventListener("click", myPrev);
+  function myPrev() {
+    $('.menu-button').toggleClass("cross bgBlack");
+  }
 }
 
-function init(){
+function init() {
   btn();
   swiper();
+  currSwiper();
 }
 window.onload = init;
+
